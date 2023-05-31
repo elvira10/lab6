ffe459a43f82883f6c75fbeff48680b76da43e69
Elvira <adilovaelvira123@gmail.com>
2023-05-31 22:57:19 +0600

    WeightedGraph addVertex

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -1,10 +1,13 @@
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import java.util.ArrayList;
 public class WeightedGraph<V> {
     private Map<Vertex<V>, List<Vertex<V>>> adjacencyMap;
     public WeightedGraph() {
         this.adjacencyMap = new HashMap<>();
+    }
+    public void addVertex(Vertex<V> vertex) {
+        adjacencyMap.put(vertex, new ArrayList<>());
     }
 }
